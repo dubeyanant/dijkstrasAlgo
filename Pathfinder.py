c26ab3ae4d1d82a10fd5401c30732b0a00da56a4
Anant Dubey <anantkumar312@gmail.com>
2023-04-05 11:24:59 +0530

    updated pathfinder.py

--- Pathfinder.py
+++ Pathfinder.py
@@ -21,7 +21,6 @@ class Cell:
         self.coords = coords
         self.visited = visited
         self.predecessor = predecessor
-    
 
     def __lt__(self, other_cell):
         return self.distance_from_start < other_cell.distance_from_start
@@ -63,13 +62,14 @@ def draw_grid():
 
     for x in range(NUMBER_OF_ROWS):
         for y in range(NUMBER_OF_COLUMNS):
-            rect = pygame.Rect((MARGIN + DIMENSION) * y + MARGIN, (MARGIN + DIMENSION) * x + MARGIN, DIMENSION, DIMENSION)
+            rect = pygame.Rect((MARGIN + DIMENSION) * y + MARGIN, (MARGIN + DIMENSION) * x + MARGIN, DIMENSION,
+                               DIMENSION)
             pygame.draw.rect(screen, WHITE, rect)
             matrix[x][y] = Cell(rect.copy(), False, np.inf, (x, y))
-    
+
     start_coords = (np.random.randint(0, NUMBER_OF_ROWS), np.random.randint(0, NUMBER_OF_COLUMNS))
     end_coords = (np.random.randint(0, NUMBER_OF_ROWS), np.random.randint(0, NUMBER_OF_COLUMNS))
-    
+
     while start_coords == end_coords:
         end_coords = (np.random.randint(0, NUMBER_OF_ROWS), np.random.randint(0, NUMBER_OF_COLUMNS))
 
@@ -84,7 +84,7 @@ def draw_grid():
 
     matrix[start_coords[0]][start_coords[1]] = Cell(start_rect.copy(), True, 0, start_coords, visited=True)
     matrix[end_coords[0]][end_coords[1]] = Cell(end_rect.copy(), False, np.inf, end_coords)
-    
+
     pygame.display.update()
 
 
@@ -100,7 +100,7 @@ def mark_as_wall():
         matrix[row][col].is_wall = True
         rect = matrix[row][col].figure
         pygame.draw.rect(screen, BLACK, rect)
-        
+
         pygame.display.update()
 
 
@@ -117,9 +117,9 @@ def mark_as_visited(cell, distance, predecessor):
             pygame.draw.rect(screen, YELLOW, rect)
 
             pygame.display.update()
-            
+
             return False
-    
+
     return True
 
 
@@ -141,26 +141,26 @@ def find_path():
     start = matrix[start_coords[0]][start_coords[1]]
     queue = [start]
     found = True
-    
+
     while queue:
         most_near = heapq.heappop(queue)
-        
+
         if most_near.distance_from_start == np.inf:
             found = False
             break
 
         i, j = most_near.coords
         new_distance = most_near.distance_from_start + 1
-        above = matrix[i-1][j] if 0 <= i-1 < NUMBER_OF_ROWS else None
-        below = matrix[i+1][j] if 0 <= i+1 < NUMBER_OF_ROWS else None
-        right = matrix[i][j+1] if 0 <= j+1 < NUMBER_OF_COLUMNS else None
-        left = matrix[i][j-1] if 0 <= j-1 < NUMBER_OF_COLUMNS else None
+        above = matrix[i - 1][j] if 0 <= i - 1 < NUMBER_OF_ROWS else None
+        below = matrix[i + 1][j] if 0 <= i + 1 < NUMBER_OF_ROWS else None
+        right = matrix[i][j + 1] if 0 <= j + 1 < NUMBER_OF_COLUMNS else None
+        left = matrix[i][j - 1] if 0 <= j - 1 < NUMBER_OF_COLUMNS else None
 
         if above and not above.is_wall and not above.visited:
             if mark_as_visited(above, new_distance, most_near):
                 break
             heapq.heappush(queue, above)
-        
+
         if below and not below.is_wall and not below.visited:
             if mark_as_visited(below, new_distance, most_near):
                 break
@@ -171,19 +171,18 @@ def find_path():
                 break
             heapq.heappush(queue, right)
 
-        if left and not left.is_wall and not left.visited: 
+        if left and not left.is_wall and not left.visited:
             if mark_as_visited(left, new_distance, most_near):
                 break
             heapq.heappush(queue, left)
-        
+
         if all(value for value in [above != None, below != None, right != None, left != None]):
             if all(value for value in [above.is_wall, below.is_wall, right.is_wall, left.is_wall]):
                 return
 
-    
     if found:
         highlight_path()
-    
+
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 matrix = None
@@ -201,20 +200,20 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-        
+
         if event.type == MOUSEBUTTONDOWN:
             dragging = True
-        
+
         if dragging and event.type == MOUSEMOTION:
             mark_as_wall()
-        
+
         if event.type == MOUSEBUTTONUP:
             dragging = False
-        
+
         if event.type == KEYDOWN:
             if event.key == K_RETURN:
                 find_path()
             if event.key == K_BACKSPACE:
                 setup()
-       
+
 pygame.quit()
